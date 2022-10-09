b57b224ac211f45fd3781cee86b0e13369bcdc2d
Ilfir <ilfir803@gmail.com>
2022-10-09 20:45:04 +0500

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+'use strict';
+// let person = {
+//    name: 'Jhon',
+//    age: 25,
+//    isMarried: false,
+// }
+
+// console.log (person.name)
+
+// let answer = confirm("Как дела?")
+// console.log(answer)
+
+// let answer = +prompt("сколько вам лет?", "")
+// console.log(answer)
+// console.log(typeof(answer))
+
+// let incr = 10,
+//    decr = 180;
+
+// console.log(++incr);
+// console.log(decr--);
+// console.log(decr)
+
+
