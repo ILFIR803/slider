b3c3068860d2a307350ee4490b230bd1359a2ed7
Ilfir <ilfir803@gmail.com>
2022-10-10 21:58:49 +0500

    циклы добавил

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 'use strict';
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -11,12 +11,42 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
 
-alert(appData.budget / 30);
+for (let i=0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+		if ((typeof(a)) === 'string' && typeof(a) != null && (typeof(b)) != null && a != '' &&  b!= '' && a.length < 50) {
+			appData.expenses[a] = b;
+		} else {
+		
+		}
+}
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+// if (appData.moneyPerDay < 100) {
+// 	console.log("Низкий доход");
+// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
+// 	console.log("Средний доход");
+// } else if (appData.moneyPerDay > 2000 ) {
+// 	console.log("Высокий доход");
+// } else {
+// 	console.log("Ошибка ввода данных");
+// }
+
+switch (appData.moneyPerDay) {
+	case appData.moneyPerDay < 100:
+		console.log("Низкий доход");
+		break;
+	case appData.moneyPerDay > 100 && appData.moneyPerDay < 2000:
+		console.log("Средний доход");
+		break;
+	case appData.moneyPerDay > 2000:
+		console.log("Высокий доход");
+		break;
+	default: 
+		console.log("Ошибка ввода данных");
+		break;
+}
